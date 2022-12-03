c1b943c78e6161b355de7a5234144487127bff4b
Hiago Crespo <112535799+heeydev@users.noreply.github.com>
2022-12-03 01:44:21 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/heeydev/Meus-Projetos/master/Todos%20os%20Materiais/Javascript/Portfolio/data/profile.json';
+    const url = 'https://github.com/heeydev/Portfolio/blob/main/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 }
